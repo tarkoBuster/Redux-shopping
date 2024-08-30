b7bba74059625dc2d86fa949395189c002893627
tarkoBuster <you@example.com>
2024-08-30 19:58:36 +0300

    add fetch cart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
-    const isCartVisible =useSelector((state) => state.main.isCartVisible);
-  return (
-    <Layout>
-        {isCartVisible && <Cart />}
-      <Products />
-    </Layout>
-  );
+    const isCartVisible = useSelector((state) => state.main.isCartVisible);
+    const cart = useSelector((state) => state.cart);
+
+    useEffect(() => {
+        fetch(
+            'https://shopping-5652e-default-rtdb.firebaseio.com/cart.json',
+            {
+                method: 'PUT',
+                body: JSON.stringify(cart),
+            }
+        );
+    }, [cart]);
+
+    return (
+        <Layout>
+            {isCartVisible && <Cart/>}
+            <Products/>
+        </Layout>
+    );
 }
 
 export default App;
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,49 +1,49 @@
 import Card from "../UI/Card";
 import styles from "./ProductItem.module.css";
-import {useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import {cartActions} from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const cart = useSelector((state) => state.cart);
+  // const cart = useSelector((state) => state.cart);
 
   const { id, title, price, description } = props;
 
   const dispatchFunction = useDispatch();
   const addItemHandler = () => {
-    const updatedItemsQuantity = cart.itemsQuantity + 1;
+    // const updatedItemsQuantity = cart.itemsQuantity + 1;
+    //
+    // const updatedItems = cart.items.slice();
+    // const existingItem = updatedItems.find((item) => item.id === id);
+    //
+    // if(existingItem) {
+    //   const updatedExistingItem = { ...existingItem};
+    //   updatedExistingItem.quantity++;
+    //   updatedExistingItem.totalPrice = updatedExistingItem.totalPrice + price;
+    //
+    //   const existingItemIndex = updatedItems.findIndex((item) => item.id === id);
+    //   updatedItems[existingItemIndex] = updatedExistingItem;
+    // } else {
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     title: title,
+    //   });
+    // }
+    //
+    // const updatedCart = {
+    //   itemsQuantity: updatedItemsQuantity,
+    //   items: updatedItems,
+    // }
+    //
+    // dispatchFunction(cartActions.updateCart(updatedCart));
 
-    const updatedItems = cart.items.slice();
-    const existingItem = updatedItems.find((item) => item.id === id);
-
-    if(existingItem) {
-      const updatedExistingItem = { ...existingItem};
-      updatedExistingItem.quantity++;
-      updatedExistingItem.totalPrice = updatedExistingItem.totalPrice + price;
-
-      const existingItemIndex = updatedItems.findIndex((item) => item.id === id);
-      updatedItems[existingItemIndex] = updatedExistingItem;
-    } else {
-      updatedItems.push({
-        id: id,
-        price: price,
-        quantity: 1,
-        totalPrice: price,
-        title: title,
-      });
-    }
-
-    const updatedCart = {
-      itemsQuantity: updatedItemsQuantity,
-      items: updatedItems,
-    }
-
-    dispatchFunction(cartActions.updateCart(updatedCart));
-
-    // dispatchFunction(cartActions.addItem({
-    //   id,
-    //   title,
-    //   price,
-    // }));
+    dispatchFunction(cartActions.addItem({
+      id,
+      title,
+      price,
+    }));
   };
 
   return (
